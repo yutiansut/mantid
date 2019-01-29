378f7940b3a5ebd83bc7645c3f622f3fb11ac552
Steven Hahn <hahnse@ornl.gov>
2019-01-29 14:32:35 -0500

    Add unit test stub.

---
+++ Framework/PythonInterface/test/python/plugins/algorithms/VisionLoadMultipleFilesTest.py
@@ -0,0 +1,18 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+# from mantid.api import
+from mantid.simpleapi import VisionLoadMultipleFiles, config, mtd
+
+
+class VisionLoadMultipleFilesTest(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
